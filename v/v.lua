38091761f5dbe13d96c3168e1c13d492d037c9d5
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2019-10-26 11:23:29 +0200

    V: Refactored

--- v/v.lua
+++ v/v.lua
@@ -1,25 +1,31 @@
-local string = require('string')
-local ui = require('ui')
+local command = require('core.command')
+local list = require('list')
+local ui = require('core.ui')
 local util = require('util')
 
-account = require('account')
-equipment = require('equipment')
-items = require('items')
-linkshell = require('linkshell')
-memory = require('memory')
-player = require('player')
-target = require('target')
-treasure = require('treasure')
-world = require('world')
+local expressions = list()
 
-local expressions = {
-}
+do
+    local v = command.new('v')
 
-for i, expression in ipairs(expressions) do
-    expressions[i] = {
-        label = expression,
-        getter = loadstring('return ' .. expression),
-    }
+    local add = function(expression)
+        expressions:add({
+            label = expression,
+            getter = loadstring('return ' .. expression),
+        })
+    end
+
+    local remove = function(index)
+        expressions:remove(index)
+    end
+
+    local clear = function()
+        expressions:clear()
+    end
+
+    v:register('add', add, '<expression:text>')
+    v:register('remove', remove, '<index:integer>')
+    v:register('clear', clear)
 end
 
 local window_state = {
@@ -31,14 +37,35 @@ local window_state = {
     width = 324,
 }
 
+setmetatable(_G, {
+    __index = function(t, k)
+        local ok, res = pcall(require, k)
+        if not ok then
+            return nil
+        end
+
+        rawset(t, k, res)
+        return res
+    end,
+})
+
 ui.display(function()
     window_state = ui.window('v', window_state, function()
-        for _, expression in ipairs(expressions) do
+        for index, expression in pairs(expressions) do
             local ok, res = pcall(expression.getter)
+            if not ok then
+                res = 'Error evaluating expression!'
+            end
+
             if type(res) == 'table' or type(res) == 'cdata' then
                 res = util.vstring(res)
             end
-            ui.text(expression.label .. ': ' .. (ok and res or '-'))
+
+            if res == nil then
+                res = 'nil'
+            end
+
+            ui.text('[' .. tostring(index) .. '] ' .. expression.label .. ':\n    ' .. res)
         end
     end)
 end)
