5348ed54c493edc42e9be0999853f96d9bc75e92
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2022-01-17 13:00:42 +0100

    V: Updated to new API, minor display improvement

--- v/v.lua
+++ v/v.lua
@@ -28,15 +28,6 @@ do
     v:register('clear', clear)
 end
 
-local window_state = {
-    title = 'Watched values',
-    style = 'normal',
-    x = 1000,
-    y = 0,
-    height = 400,
-    width = 295,
-}
-
 setmetatable(_G, {
     __index = function(t, k)
         local ok, res = pcall(require, k)
@@ -49,23 +40,33 @@ setmetatable(_G, {
     end,
 })
 
+local window_state = ui.window_state()
+window_state.title = 'Watched values'
+window_state.style = 'standard'
+window_state.resizable = true
+window_state.position.x = 1000
+window_state.position.y = 0
+window_state.size.height = 400
+window_state.size.width = 295
+
 ui.display(function()
-    window_state = ui.window('v', window_state, function()
+    ui.window(window_state, function(layout)
         for index, expression in pairs(expressions) do
             local ok, res = pcall(expression.getter)
             if not ok then
-                res = 'Error evaluating expression!'
+                res = 'Error evaluating expression!' .. res
+            else
+                if type(res) == 'table' or type(res) == 'cdata' then
+                    res = util.vstring(res)
+                end
+
+                if res == nil then
+                    res = 'nil'
+                end
             end
 
-            if type(res) == 'table' or type(res) == 'cdata' then
-                res = util.vstring(res)
-            end
-
-            if res == nil then
-                res = 'nil'
-            end
-
-            ui.text('[' .. tostring(index) .. '] ' .. expression.label .. ':\n    ' .. tostring(res))
+            layout:width(10000)
+            layout:label('[' .. tostring(index) .. '] ' .. expression.label .. ':\n    ' .. tostring(res))
         end
     end)
 end)
